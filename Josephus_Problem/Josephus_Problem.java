033376e02b6911c5c000e8645baada851e25437e
Abhiraj Kale <abhirajkale1806@gmail.com>
2020-08-19 16:15:18 +0530

    New problem

--- Josephus_Problem/Josephus_Problem.java
+++ Josephus_Problem/Josephus_Problem.java
@@ -1,4 +1,12 @@
 package Problem_statements.Josephus_Problem;
+/* Problem Statement
 
+Josephus problem
+There are n people standing in a queue waiting to be executed. The counting begins at the front of
+the queue. In each step, k number of people are removed and again add one by one from the
+queue. Then the next person is executed. The execution proceeds around the circle until only the
+last person remains, who is given freedom.
+Find that position where you want to stand and gain your freedom.
+* */
 public class Josephus_Problem {
 }
